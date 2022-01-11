65630c33502237c1d69f4c1beed62ae70497a321
Valerio Corda <corda.valerio05@GMAIL.COM>
2022-01-11 18:36:41 +0100

    add footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,12 @@
+<footer>
+  <div class="logocontainer">
+    <img src="https://picsum.photos/200/300" alt="">
+  </div>
+  <nav>
+    <ul>
+      <li>footer</li>
+      <li>footer</li>
+      <li>footer</li>
+    </ul>
+  </nav>
+</footer>
