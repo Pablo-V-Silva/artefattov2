ab1740ded628c3a47b44727804706969f8482c2b
Pablo-V-Silva <booleanpablosilva@gmail.com>
2022-01-11 18:12:03 +0100

    Add Header sheet

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+  <div class="logocontainer">
+    <!-- <img src="" alt=""> -->
+  </div>
+  <nav>
+    <ul>
+      <li>lorem</li>
+      <li>lorem</li>
+      <li>lorem</li>
+    </ul>
+  </nav>
+</header>
